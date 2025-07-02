07edad0fc4a3ab2b098c83003fb5fdb9c807b6f0
Lin Bai <lbai@qti.qualcomm.com>
2025-07-02 09:25:07 +0800

    cnss2: Ignore spurious dev_sol during POWERING_ON
    Ignore spurious dev_sol IRQ during POWERING_ON,
    instead of delay sol initialization in
    commit d23dcbe265f3 ("cnss2: Delay sol irq registration to server arrive").
    
    Change-Id: I0c907447844a29c4ad98e6e3c143fe269ae4a1dc
    CRs-Fixed: 4200972
    (cherry picked from commit bc7384aae2785b112f73410e9a10fff2077cf6bf)

--- cnss2/main.c
+++ cnss2/main.c
@@ -2108,7 +2108,7 @@ static void cnss_deinit_host_sol_gpio(struct cnss_plat_data *plat_priv)
 	gpio_free(sol_gpio->host_sol_gpio);
 }
 
-int cnss_init_sol_gpio(struct cnss_plat_data *plat_priv)
+static int cnss_init_sol_gpio(struct cnss_plat_data *plat_priv)
 {
 	int ret;
 
@@ -5176,6 +5176,10 @@ static void cnss_fmd_status_update_cb(void *cb_ctx, bool status)
 static int cnss_misc_init(struct cnss_plat_data *plat_priv)
 {
 	int ret;
+
+	ret = cnss_init_sol_gpio(plat_priv);
+	if (ret)
+		return ret;
 
 	timer_setup(&plat_priv->fw_boot_timer,
 		    cnss_bus_fw_boot_timeout_hdlr, 0);
